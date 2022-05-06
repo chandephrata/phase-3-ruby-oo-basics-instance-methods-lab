88ae696550ecb68d1ff5af6fda3fec94c442f0f4
Chandelier Ephrata <chandephrata@gmail.com>
2022-05-06 15:54:34 -0600

    commit tree 1c80624b35ae795d180d4efa977be5b6b97ff0cb

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+#Class body
+
+class Dog
+
+#Instance method definition
+    def bark
+        puts "Woof!"
+    end
+
+    def sit
+        puts "The Dog is sitting"
+    end
+end
+
+fido = Dog.new
+fido.bark 
+
+snoopy = Dog.new
+snoopy.bark
